bda024ae7bb015482e90e064fd42c62d3feec0ef
zhangjiashu <694690369@qq.com>
2024-12-03 19:37:06 +0800

    first commit

---
+++ scripts/player.gd
@@ -0,0 +1,53 @@
+extends CharacterBody2D
+
+# 速度
+const SPEED = 130.0
+# 跳跃速度
+const JUMP_VELOCITY = -300.0
+
+@onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
+
+# velocity [默认： Vector2(0, 0)]
+# 当前速度向量，单位为像素每秒
+
+func _physics_process(delta: float) -> void:
+	# Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+
+	# Handle jump.
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	# 如果不按下如何按钮 direction = 0
+	# 如果按下左键 direction = -1
+	# 如果按下右键 direction = 1
+	var direction := Input.get_axis("move_left", "move_right")
+	
+	# 翻转精灵
+	if direction > 0:
+		animated_sprite.flip_h = false
+	elif direction < 0:
+		animated_sprite.flip_h = true
+		
+	# 判断是否在地面上
+	if is_on_floor():
+		# 如果没有操作
+		if direction == 0:
+			# 播放闲置动画
+			animated_sprite.play("idle")
+		else:
+			# 播放奔跑动画
+			animated_sprite.play("run")		
+	# 离开地面
+	else:
+		# 播放跳跃动画
+		animated_sprite.play("jump")	
+
+	
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+
+	move_and_slide()
